90f81ca3a2f65710710160854c35a9172489c22a
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 03:31:43 -0500

    Update concert.services.js

--- src/concerts/services/concert.services.js
+++ src/concerts/services/concert.services.js
@@ -1,15 +1,21 @@
 import { Concert } from '@/concerts/model/concert.entity';
 
 export class ConcertService {
+  // En producción, debe ser '/db.json'
   resourceEndpoint = import.meta.env.VITE_CONCERTS_ENDPOINT_PATH || '/db.json';
 
   async getAll() {
     try {
       const response = await fetch(this.resourceEndpoint);
+
+      if (!response.ok) {
+        throw new Error(`❌ No se pudo cargar el archivo JSON: ${response.status}`);
+      }
+
       const json = await response.json();
 
       if (!json.concerts || !Array.isArray(json.concerts.data)) {
-        console.error('❌ Estructura inválida en json.concerts.data');
+        console.error('❌ Estructura inválida: json.concerts.data');
         return [];
       }
 
